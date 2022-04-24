d0902c8d710b566f1958ec258ab7cfc5899dde47
AsotikovaSvetlana <workasotikova@gmail.com>
2022-04-24 21:43:33 +0300

    fix path

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/gallery/:id" element={<Photo />} />
+          <Route path="/images/:id" element={<Photo />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </main>
